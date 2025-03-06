7ef15790b474b85e8246f345701c3215111f38db
rennerparlato473 <rennerparlato473@gmail.com>
2025-03-06 13:40:08 +0000

    Add 1.ts

---
+++ src/1.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schoolproject',
+  templateUrl: './schoolproject.component.html',
+  styleUrls: ['./schoolproject.component.css']
+})
+export class SchoolProjectComponent {
+  constructor() {}
+}
