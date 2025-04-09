2adc4591f67f1a4cd12f3b79d24b1ebae32f1aa1
rennerparlato473 <rennerparlato473@gmail.com>
2025-04-09 10:02:18 +0000

    Add 28.ts

---
+++ src/28.ts
@@ -0,0 +1,10 @@
+function isPrime(num: number): boolean {
+    if (num <= 1) return false;
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+console.log(isPrime(7)); // true
+console.log(isPrime(10)); // false
